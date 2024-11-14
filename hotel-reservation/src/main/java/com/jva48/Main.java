6057f27dc42b23604b203f48743f7918e9332672
hksnll <jungyerin828@gmail.com>
2024-11-14 12:33:17 +0800

    commit

--- hotel-reservation/src/main/java/com/jva48/Main.java
+++ hotel-reservation/src/main/java/com/jva48/Main.java
@@ -1,8 +1,13 @@
 package com.jva48;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        
+        Scanner scanner = new  Scanner(System.in);
+        String input = scanner.nextLine();
+
+        System.out.println(input);
+        scanner.close();
     }
 }
